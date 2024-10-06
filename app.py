6941203293cd9859e96312551ce62dd7ba4dcf6d
deepakkapse <41117353+deepakkapse@users.noreply.github.com>
2024-10-06 23:23:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import os
 
@@ -23,11 +23,11 @@ db = SQLAlchemy(app)
 
 from models import EmployeeModel
 
-@app.before_first_request
-def create_table():
+# Create tables on app initialization
+with app.app_context():
     db.create_all()
 
-@app.route('/data/create' , methods = ['GET','POST'])
+@app.route('/data/create', methods=['GET', 'POST'])
 def create():
     if request.method == 'GET':
         return render_template('templates/createpage.html')
@@ -37,27 +37,26 @@ def create():
         name = request.form['name']
         age = request.form['age']
         position = request.form['position']
-        employee = EmployeeModel(employee_id=employee_id, name=name, age=age, position = position)
+        employee = EmployeeModel(employee_id=employee_id, name=name, age=age, position=position)
         db.session.add(employee)
         db.session.commit()
-        return redirect('/data')
-
+        return redirect(url_for('datalist'))
 
 @app.route('/data')
-def RetrieveList():
+def datalist():
     employees = EmployeeModel.query.all()
-    return render_template('templates/datalist.html',employees = employees)
+    return render_template('templates/datalist.html', employees=employees)
 
 
 @app.route('/data/<int:id>')
-def RetrieveEmployee(id):
+def retrieve_employee(id):
     employee = EmployeeModel.query.filter_by(employee_id=id).first()
     if employee:
-        return render_template('templates/data.html', employee = employee)
-    return f"Employee with id ={id} Doenst exist"
+        return render_template('templates/data.html', employee=employee)
+    return f"Employee with id = {id} Does not exist"
 
 
-@app.route('/data/<int:id>/update',methods = ['GET','POST'])
+@app.route('/data/<int:id>/update', methods=['GET', 'POST'])
 def update(id):
     employee = EmployeeModel.query.filter_by(employee_id=id).first()
     if request.method == 'POST':
@@ -67,24 +66,24 @@ def update(id):
             name = request.form['name']
             age = request.form['age']
             position = request.form['position']
-            employee = EmployeeModel(employee_id=id, name=name, age=age, position = position)
+            employee = EmployeeModel(employee_id=id, name=name, age=age, position=position)
             db.session.add(employee)
             db.session.commit()
-            return redirect(f'/data/{id}')
-        return f"Employee with id = {id} Does nit exist"
+            return redirect(url_for('retrieve_employee', id=id))
+        return f"Employee with id = {id} Does not exist"
 
-    return render_template('templates/update.html', employee = employee)
+    return render_template('templates/update.html', employee=employee)
 
 
-@app.route('/data/<int:id>/delete', methods=['GET','POST'])
+@app.route('/data/<int:id>/delete', methods=['GET', 'POST'])
 def delete(id):
     employee = EmployeeModel.query.filter_by(employee_id=id).first()
     if request.method == 'POST':
         if employee:
             db.session.delete(employee)
             db.session.commit()
-            return redirect('/data')
-        abort(404)
+            return redirect(url_for('datalist'))
+        return abort(404)
 
     return render_template('templates/delete.html')
 
